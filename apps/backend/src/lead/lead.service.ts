d17ed6c2b1ca22f9908cdd783412689df13f43be
MAG1980 <al-1980@inbox.ru>
2024-06-23 15:22:26 +0300

    В ответ API добавлены данные о контактах, прикреплённых к сделкам.

--- apps/backend/src/lead/lead.service.ts
+++ apps/backend/src/lead/lead.service.ts
@@ -1,30 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from "@nestjs/config";
-import {GetLeadsDto} from "./dto/get-leads.dto";
+import { ConfigService } from '@nestjs/config';
+import { GetLeadsDto } from './dto/get-leads.dto';
 
 @Injectable()
 export class LeadService {
   constructor(private readonly configService: ConfigService) {}
 
-  async fetchLeads(getLeadssDto:GetLeadsDto) {
-    const { query } = getLeadssDto
+  async fetchLeads(getLeadsDto: GetLeadsDto) {
+    const { query } = getLeadsDto;
 
-    const params = query ? `?query=${query}` : ''
+    const defaultParams = '?with=contacts';
 
-    getLeadssDto.query = getLeadssDto.query || ''
-    
-    const domain =
-      this.configService.getOrThrow('API_DOMAIN')
+    const params = query
+      ? `${defaultParams}&query=${query}`
+      : `${defaultParams}`;
 
-    const url = `https://${domain}/api/v4/leads${params}`
+    const domain = this.configService.getOrThrow('API_DOMAIN');
 
-    const res = await fetch(url,{
+    const url = `https://${domain}/api/v4/leads${params}`;
+
+    const res = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${this.configService.getOrThrow('LONG_TERM_TOKEN')}`
-      }
-    })
-    return await res.json()
+        Authorization: `Bearer ${this.configService.getOrThrow('LONG_TERM_TOKEN')}`,
+      },
+    });
+    return await res.json();
   }
 }
