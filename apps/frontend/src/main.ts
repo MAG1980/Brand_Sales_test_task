c4c7823e286f346169bacaeb1eb2123326aca1f4
MAG1980 <al-1980@inbox.ru>
2024-06-25 03:53:17 +0300

    Подключена библиотека Ant Design.

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,6 +1,8 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(Antd).mount('#app')
